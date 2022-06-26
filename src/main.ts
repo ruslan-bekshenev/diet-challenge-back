47136686e0ceb17932678a28c7983e3e83fa4af8
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-06-26 19:43:05 +0500

    cors setting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+  const corsOptions: CorsOptions = {
+    allowedHeaders: [
+      'origin',
+      'x-requested-with',
+      'content-type',
+      'accept',
+      'authorization',
+    ],
+    credentials: true,
+    origin: ['http://localhost:3000', 'http://localhost:3000'],
+  };
+  app.enableCors(corsOptions);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT || 3000);
